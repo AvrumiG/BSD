d5591367aa6c8c8d1f81eef4e3138fb5f0eb883a
avrumig <agross3179@gmail.com>
2024-10-08 21:05:44 +0100

    code review see comments in file

--- 2 sample data/2 Sample data.sql
+++ 2 sample data/2 Sample data.sql
@@ -1,7 +1,8 @@
+--AG please populate sample data for planet
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'muoth', 2
+union select 'muoth', 2 --there seem to be an unnoticed change with the spelling of mouth
 union select 'teeth', 32
 union select 'toungue', 1
